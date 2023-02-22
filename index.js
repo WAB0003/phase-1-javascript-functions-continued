28546fd8c0b43443ef65e7aa05785ad356cccf8e
Bill Brown <bill.a.brown90@gmail.com>
2023-02-22 11:51:06 -0800

    Using functions as declared and nested within

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+//using function declaration
+//function declaration is useful because one it's been declared, you can call the function above or below the line of code.
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(workType = 'go to the office') {
+    return `This Monday, I will ${workType}.`
+}
+
+function wrapAdjective(flareSymbol) {
+    return function(phrase) {
+        const intro = "You are "
+        return intro + flareSymbol + phrase + flareSymbol + "!";
+    }
+}
